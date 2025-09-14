a4f1675662e3ab82804ff51833553348ec7fff15
someone-s <workemaileden@gmail.com>
2025-09-14 17:05:04 +0100

    add initial create table

--- manage/server.py
+++ manage/server.py
@@ -1,5 +1,6 @@
 from typing import Annotated
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
+from pydantic import BaseModel, Field
 
 import os
 import time
@@ -24,6 +25,7 @@ while conn == None:
 
 conn.execute('CREATE EXTENSION IF NOT EXISTS vector')
 register_vector(conn)
+conn.execute(f'CREATE TABLE IF NOT EXISTS chunks (id bigserial PRIMARY KEY, content text, embedding vector({vector_dimension}))')
 
 app = FastAPI()
 
@@ -32,15 +34,22 @@ def wipe():
     conn.execute('DROP TABLE IF EXISTS chunks')
     conn.execute(f'CREATE TABLE chunks (id bigserial PRIMARY KEY, content text, embedding vector({vector_dimension}))')
 
+
+
+class AddRequestItem(BaseModel):
+    chunk_content: str = Field(description="String representing the chunk text")
+    embed: list[int] = Field(description="Array of int representing the embed vector")
+
 @app.post('/add')
-def add(\
-        chunk_content: Annotated[str, File(description="String representing the chunk text")],
-        embed: Annotated[list[int], File(description="Array of int representing the embed vector")]):
-    embedding = np.array(embed)
-    conn.execute('INSERT INTO chunks (content, embedding) VALUES (%s, %s)', (chunk_content, embedding,))
+def add(item: AddRequestItem):
+    embedding = np.array(item.embed)
+    conn.execute('INSERT INTO chunks (content, embedding) VALUES (%s, %s)', (item.chunk_content, embedding,))
+
+class FetchRequestItem(BaseModel):
+    embed: list[int] = Field(description="Array of int representing the embed vector")
 
 @app.post('/fetch')
-def fetch(embed: Annotated[list[int], File(description="Array of int representing the embed vector")]):
-    embedding = np.array(embed)
+def fetch(item: FetchRequestItem):
+    embedding = np.array(item.embed)
     result = conn.execute('SELECT content FROM chunks ORDER BY embedding <-> %s LIMIT 5', (embedding,)).fetchall()
     return [row[0] for row in result]
