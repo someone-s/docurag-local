78d5e74f2d0ca18724d31791bca5ab858c6436b0
someone-s <workemaileden@gmail.com>
2025-09-17 21:41:16 +0100

    adjust font size

--- frontend/src/components/ui/pdf/PDFControl.vue
+++ frontend/src/components/ui/pdf/PDFControl.vue
@@ -18,7 +18,7 @@ const { provides: scrollProvides, state: scrollState } = useScroll();
       <Button variant="ghost" size="icon" @click="scrollProvides.scrollToPreviousPage()">
         <ChevronLeftIcon class="w-4 h-4"/>
       </Button>
-      <span class="w-14 h-9 flex items-center  justify-center">
+      <span class="w-14 h-9 flex items-center  justify-center text-sm">
         {{scrollState.currentPage}}/{{scrollState.totalPages}}
       </span>
       <Button variant="ghost" size="icon" @click="scrollProvides.scrollToNextPage()">
@@ -29,7 +29,7 @@ const { provides: scrollProvides, state: scrollState } = useScroll();
       <Button variant="ghost" size="icon" @click="zoomProvides.zoomIn()">
         <ZoomInIcon class="w-4 h-4"/>
       </Button>
-      <span class="w-14 h-9 flex items-center  justify-center">
+      <span class="w-14 h-9 flex items-center  justify-center text-sm">
         {{Math.round(zoomState.currentZoomLevel * 100)}}%
       </span>
       <Button variant="ghost" size="icon" @click="zoomProvides.zoomOut()">
