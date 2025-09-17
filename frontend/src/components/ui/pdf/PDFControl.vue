f98a68b19732d7380254b602cb787aa823751d79
someone-s <workemaileden@gmail.com>
2025-09-17 21:27:21 +0100

    add drag functionality

--- frontend/src/components/ui/pdf/PDFControl.vue
+++ frontend/src/components/ui/pdf/PDFControl.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { ChevronLeftIcon, ChevronRightIcon, ZoomInIcon, ZoomOutIcon, FullscreenIcon } from "lucide-vue-next";
+import { ChevronLeftIcon, ChevronRightIcon, ZoomInIcon, ZoomOutIcon, HandGrabIcon, HandIcon } from "lucide-vue-next";
 import { Button } from "@/components/ui/button";
-import { useZoom, ZoomMode } from '@embedpdf/plugin-zoom/vue';
+import { Toggle } from "@/components/ui/toggle";
+import { useZoom } from '@embedpdf/plugin-zoom/vue';
 import { useScroll } from "@embedpdf/plugin-scroll/vue";
+import { usePan } from "@embedpdf/plugin-pan/vue";
  
 // The composable provides reactive state and methods
 const { provides: zoomProvides, state: zoomState } = useZoom();
 
 // Provide scroll functionality
 const { provides: scrollProvides, state: scrollState } = useScroll();
+
+// Provide pan functionality
+const { provides: panProvides, isPanning } = usePan()
 </script>
  
 <template>
-  <div v-if="zoomProvides && scrollProvides" class="relative w-full bottom-12 flex  justify-center">
+  <div v-if="zoomProvides && scrollProvides && panProvides" class="relative w-full bottom-12 flex  justify-center">
 
     <div class='inline-flex w-fit border rounded-md shadow-xs bg-background mr-2'>
       <Button variant="ghost" size="icon" @click="scrollProvides.scrollToPreviousPage()">
@@ -36,9 +41,10 @@ const { provides: scrollProvides, state: scrollState } = useScroll();
         <ZoomOutIcon class="w-4 h-4"/>
       </Button>
     </div>
-    <Button variant="outline" size="icon" @click="zoomProvides.requestZoom(ZoomMode.FitPage)">
-      <FullscreenIcon class="w-4 h-4" />
-    </Button>
+    <Toggle variant="outline" class="bg-background" :model-value="isPanning" v-on:update:model-value="(state) => { if (state) panProvides?.enablePan(); else panProvides?.disablePan(); }">
+      <HandGrabIcon v-if="isPanning" class="w-4 h-4" />
+      <HandIcon v-else class="w-4 h-4" />
+    </Toggle>
     
   </div>
 </template>
--- frontend/src/components/ui/pdf/PDFViewer.vue
+++ frontend/src/components/ui/pdf/PDFViewer.vue
@@ -2,7 +2,7 @@
 import { usePdfiumEngine } from '@embedpdf/engines/vue';
 import { EmbedPDF } from '@embedpdf/core/vue';
 import { createPluginRegistration } from '@embedpdf/core';
- 
+
 // Import the essential plugins and their components
 import { ViewportPluginPackage, Viewport } from '@embedpdf/plugin-viewport/vue';
 import { Scroller, ScrollPluginPackage } from '@embedpdf/plugin-scroll/vue';
@@ -16,11 +16,14 @@ import PDFSelect from './PDFSelect.vue';
 import type { PDFDocument } from './PDFDocument';
 import { ref } from 'vue';
 
+import { GlobalPointerProvider, InteractionManagerPluginPackage } from '@embedpdf/plugin-interaction-manager/vue'
+import { PanPluginPackage } from '@embedpdf/plugin-pan/vue';
+
 const componentKey = ref(0);
- 
+
 // 1. Initialize the engine with the Vue composable
 const { engine, isLoading } = usePdfiumEngine();
- 
+
 // 2. Register the plugins you need
 const plugins = [
   createPluginRegistration(LoaderPluginPackage),
@@ -31,6 +34,10 @@ const plugins = [
     // Set the initial zoom level when a document loads
     defaultZoomLevel: ZoomMode.FitPage,
   }),
+  createPluginRegistration(InteractionManagerPluginPackage),
+  createPluginRegistration(PanPluginPackage, {
+    defaultMode: 'always'
+  })
 ];
 
 
@@ -38,39 +45,47 @@ const props = defineProps<{
   documents: PDFDocument[]
 }>();
 </script>
- 
+
 <template>
   <div class="relative">
     <div v-if="isLoading || !engine" class="loading-pane">
       Loading PDF Engine...
     </div>
-    
+
     <EmbedPDF v-else :engine="engine" :plugins="plugins">
-      <Viewport class="viewport-class no-scrollbar bg-background" :key="componentKey">
-        <Scroller>
-          <template #default="{ page }">
-            <div
-              :style="{
+      <GlobalPointerProvider>
+        <Viewport class="viewport-class no-scrollbar bg-background" :key="componentKey">
+          <Scroller>
+            <template #default="{ page }">
+              <div :style="{
                 width: `${page.width}px`,
                 height: `${page.height}px`,
-              }"
-            >
-              <RenderLayer :pageIndex="page.pageIndex" />
-            </div>
-          </template>
-        </Scroller>
-      </Viewport>
+              }">
+                <RenderLayer :pageIndex="page.pageIndex" class="no-drag"/>
+              </div>
+            </template>
+          </Scroller>
+        </Viewport>
+      </GlobalPointerProvider>
       <PDFSelect :documents="props.documents" :reload="() => componentKey++"></PDFSelect>
       <PDFControl></PDFControl>
     </EmbedPDF>
   </div>
 </template>
- 
+
 <style scoped>
 .loading-pane {
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100%;
+}
+
+.no-drag {
+    -webkit-user-drag: none;
+    user-select: none;
+    -moz-user-select: none;
+    -webkit-user-select: none;
+    -ms-user-select: none;
 }
 </style>
