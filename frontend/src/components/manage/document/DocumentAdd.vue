299dd981f6266e7d591b58903d26f3de6eccce09
someone-s <workemaileden@gmail.com>
2025-09-23 20:07:53 +0100

    change to popover

--- frontend/src/components/manage/document/DocumentAdd.vue
+++ frontend/src/components/manage/document/DocumentAdd.vue
@@ -1,5 +1,9 @@
 <script setup lang="ts">
-
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
 import Button from '@/components/ui/button/Button.vue';
 import { Plus } from 'lucide-vue-next';
 
@@ -7,8 +11,15 @@ import { Plus } from 'lucide-vue-next';
 </script>
 
 <template>
-  <Button class="ml-auto cursor-pointer">
-    Add
-    <Plus class="ml-2 h-4 w-4" />
-  </Button>
+  <Popover>
+    <PopoverTrigger as-child class="ml-auto cursor-pointer">
+      <Button>
+        Add
+        <Plus class="ml-2 h-4 w-4" />
+      </Button>
+    </PopoverTrigger>
+    <PopoverContent>
+      Some popover content
+    </PopoverContent>
+  </Popover>
 </template>
