88981a1cb5e6f3f2067f547b5be0c301c46acd0c
someone-s <workemaileden@gmail.com>
2025-09-28 16:47:08 +0100

    merge chunking into main backend server

--- backend/chunkconnection.py
+++ backend/chunkconnection.py
@@ -1,20 +1,27 @@
-from fastapi import HTTPException
 
-import requests
 import os
 
-chunk_serviceorip = os.environ['CHUNK_SERVICEORIP']
-chunk_port = os.environ['CHUNK_PORT']
+import semchunk
+from transformers import AutoTokenizer, PreTrainedTokenizer
 
-batch_size = int(os.environ['EMBED_BATCH_SIZE'])
+model = os.environ['MODEL_ID']
+max_size = int(os.environ['MAX_SIZE'])
+
+tokenizer: PreTrainedTokenizer
+if os.environ['PAD_LEFT'] == 'true':
+    tokenizer = AutoTokenizer.from_pretrained(model, padding_side='left')
+else:
+    tokenizer = AutoTokenizer.from_pretrained(model)
+
+default_chunker = semchunk.chunkerify(tokenizer, max_size)
 
 def get_chunk(pretense: str|None, text: str) -> list[str]:
 
-    chunK_input = { 'text': text }
-    if pretense != None:
-        chunK_input['pretense'] = pretense
-    chunk_req = requests.post(f'http://{chunk_serviceorip}:{chunk_port}/', json=chunK_input)
-    if chunk_req.status_code != 200:
-        raise HTTPException(status_code=chunk_req.status_code, detail=chunk_req.reason)
-    
-    return chunk_req.json()
+    if pretense == None:
+        chunks:list[str] = default_chunker(text, overlap=0.2)
+        return chunks
+    else:
+        pretense_token_count = len(tokenizer.tokenize(pretense))
+        offset_chunker = semchunk.chunkerify(tokenizer, max_size - pretense_token_count - 10) # 10 extra token for space between and margin of error
+        chunks:list[str] = offset_chunker(text, overlap=0.2)
+        return list(map(lambda text: f"{pretense} {text}", chunks))
